89e8843c48344ad877b787121fb5c3970b64937d
git stash <evan.t.lloyd@gmail.com>
2024-05-15 14:04:24 -0700

    index on compilation-fallback: 410f50b opaque gpt2, tox plugin to run commands_pre even if --notest

--- scripts/build_pyproject.py
+++ scripts/build_pyproject.py
@@ -4,7 +4,7 @@ import re
 [root_dir, env_name, out_name] = sys.argv[1:]
 torch_version = re.match(r"(lock|update)-(torch\d+)", env_name).group(2)
 
-with open(out_name, "a") as out_file:
+with open(out_name, "w") as out_file:
     for filename in [
         f"{root_dir}/.poetry-lockfiles/pyproject.in",
         f"{root_dir}/.poetry-lockfiles/{torch_version}.in",
