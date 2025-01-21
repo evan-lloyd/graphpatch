72a7d52ff267208849f811e608a188dc93e6c01b
Evan Lloyd <evan.t.lloyd@gmail.com>
2025-01-21 13:52:50 -0800

    continue uv migration

--- scripts/copy_lockfile.py
+++
@@ -1,9 +0,0 @@
-import sys
-import re
-
-[root_dir, env_name] = sys.argv[1:]
-torch_version = re.match(r"(lock|update)-(torch\d+)", env_name).group(2)
-
-with open(f"{root_dir}/.poetry-lockfiles/lock-{torch_version}.lock", "w") as out_file:
-    with open("poetry.lock", "r") as in_file:
-        out_file.write(in_file.read())
--- toxfile.py
+++
@@ -1,8 +0,0 @@
-from tox.tox_env.api import ToxEnv
-
-
-def _write_execute_log(*args, **kwargs):
-    return
-
-
-ToxEnv._write_execute_log = _write_execute_log
